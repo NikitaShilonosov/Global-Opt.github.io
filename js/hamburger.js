f4e87afb21586727d2c79d01ab5e61f3e24c2e95
NikitaShilonosov <149965857+NikitaShilonosov@users.noreply.github.com>
2023-12-03 16:15:16 +0300

    Add files via upload
    Добавил возможность закрытия гамбургер меню по клику на список, само меню и на крестик

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,6 +1,15 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
+let li = document.querySelector ('li');
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
+menu.addEventListener('click', () => {
+    menu.classList.remove('active');
+    menu.classList.remove('active');
+});
+li.addEventListener('click', () => {
+    menu.classList.remove('active');
+	menuBtn.classList.remove('active');
+});
